bec148f088aeb02a3a156d0894a6154c23625c7d
Lenan22 <84825347+Lenan22@users.noreply.github.com>
2022-10-18 11:26:47 +0800

    Update generate_npy.py

--- generate_npy.py
+++ generate_npy.py
@@ -7,7 +7,7 @@ result_path = "./data"
 
 if __name__ == '__main__':
     for i in tqdm(range(500)):
-        data = np.random.random((1,112,112))
+        data = np.random.random((1,3,608,608))
         x = np.array(data, dtype=np.float32)
         new_path_name = os.path.join(result_path,str(i)) + ".npy"
         np.save(new_path_name,x)
