418d0dbc2df714cefaf473c95dc608ac36fff908
Lenan22 <84825347+Lenan22@users.noreply.github.com>
2022-10-18 11:15:46 +0800

    Create generate_npy.py

---
+++ generate_npy.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pdb
+import os
+from tqdm import tqdm
+
+result_path = "./data"
+
+if __name__ == '__main__':
+    for i in tqdm(range(500)):
+        data = np.random.random((1,112,112))
+        x = np.array(data, dtype=np.float32)
+        new_path_name = os.path.join(result_path,str(i)) + ".npy"
+        np.save(new_path_name,x)
