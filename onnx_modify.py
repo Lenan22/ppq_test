732bdb1c663799e5ce605801e601d834a3aeafae
Lenan22 <cugb_wulenan@163.com>
2023-04-17 14:44:25 +0800

    aaaa

--- onnx_modify.py
+++ onnx_modify.py
@@ -11,12 +11,12 @@ import numpy as np
 
 
 #ClipPlugin for relu6 has been a built-in op in tensorrt, and we map the relu6 to CustomClipPlugin only for demo.
-def replace_top_with_customTopkPlugin(old_onnx_filename, new_onnx_filename):  
+def replace_top_with_customTopkPlugin(old_onnx_filename, new_onnx_filename):
     model = onnx.load(old_onnx_filename)
 
     graph = model.graph
     # graph.cleanup()
-    
+
     #map Clip op to CustomClipPlugin
     for i in range(len(graph.node)):
         node = graph.node[i]
@@ -24,12 +24,12 @@ def replace_top_with_customTopkPlugin(old_onnx_filename, new_onnx_filename):
         output = node.output
 
         if '594' in output:
-            node.name = "reshze_" + str(i)
-            print("---")
+            node.name = 'reshze_' + str(i)
+            print('---')
 
         if '595' in output:
-            node.name = "reshze_" + str(i)
-            print("---")
+            node.name = 'reshze_' + str(i)
+            print('---')
 
         # if node.name in topk_constant_1000:
         #     output_tensor_name = str(fname) + "_topk"
@@ -49,21 +49,19 @@ def replace_top_with_customTopkPlugin(old_onnx_filename, new_onnx_filename):
         #     node.attrs = OrderedDict({"clipMin":0.0, "clipMax":6.0})
 
     # onnx.checker.check_model(model)
-    onnx.save(model, new_onnx_filename)    
+    onnx.save(model, new_onnx_filename)
 
 
 # OrderedDict([('axis', 1), ('largest', 1), ('sorted', 1)])
 
 
 if __name__ == '__main__':
-    onnx_filename = "Retinanet_3_450_620.onnx"
+    onnx_filename = 'Retinanet_3_450_620.onnx'
     # onnx_filename = "models/new_end2end_float_static.onnx"
     # export_onnx(onnx_filename)
     # print_onnx_model(onnx_filename)
 
-    new_onnx_filename = "Retinanet_3_450_620_add_resize_name.onnx"
+    new_onnx_filename = 'Retinanet_3_450_620_add_resize_name.onnx'
     # new_onnx_filename = "models/new_end2end_float_static.onnx"
 
     replace_top_with_customTopkPlugin(onnx_filename, new_onnx_filename)
-
-
